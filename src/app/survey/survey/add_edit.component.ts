2f4f255227024c12376e44af42a130fd3f480704
zacherycardoza <85895258+zacherycardoza@users.noreply.github.com>
2021-12-11 14:56:16 -0500

    ZC add question button fix

--- src/app/survey/survey/add_edit.component.ts
+++ src/app/survey/survey/add_edit.component.ts
@@ -13,6 +13,7 @@ export class AddEditComponent {
 
     editing: boolean = false;
     survey: Survey = new Survey();
+    questionCount: number;
 
     constructor(private repository: SurveyRepository,
                 private router: Router,
@@ -41,5 +42,73 @@ export class AddEditComponent {
         this.repository.deleteSurvey(id);
         this.router.navigateByUrl("survey/mysurveys");
     }
-    
+
+    addQuestionButtonClick() {
+        if (this.questionCount == undefined) this.questionCount = 1;
+        else this.questionCount++;
+        console.log(this.questionCount);
+      // question container
+      var container = document.createElement("div");
+      container.setAttribute("class", "form-group");
+      //label
+      var qLabel = document.createElement("label");
+      qLabel.setAttribute("for", `Question${this.questionCount}`);
+      qLabel.innerHTML = `Question ${this.questionCount}.`;
+      container.appendChild(qLabel);
+      // input
+      var qInput = document.createElement("input");
+      qInput.setAttribute("type", "text");
+      qInput.setAttribute("class", "form-control");
+      qInput.setAttribute("id", "statusTextField");
+      qInput.setAttribute("placeholder", "Your Question");
+      qInput.setAttribute("name", `Question${this.questionCount}`);
+      container.appendChild(qInput);
+      // Answer Type Radios
+      var aTLabel = document.createElement("label");
+      aTLabel.innerHTML = "Answer Type";
+      aTLabel.setAttribute("for", `AnswerTypeField`);
+      container.appendChild(aTLabel);
+      container.appendChild(document.createElement("br"));
+
+      for (var i = 0; i < 5; i++) {
+        // label
+        var aLabel = document.createElement("label");
+        aLabel.setAttribute("for", `AnswerRadio${i}`);
+        var aRadioButton = document.createElement("input");
+        aRadioButton.setAttribute("type", "radio");
+        aRadioButton.setAttribute("name", `AnswerRadio${i}`);
+        aRadioButton.setAttribute(
+          "id",
+          `${this.questionCount}AnswerRadioChoice${i + 1}`
+        );
+
+        var aType;
+        switch (i) {
+          case 0:
+            aType = "True or False";
+            break;
+          case 1:
+            aType = "Scale";
+            break;
+          case 2:
+            aType = "Multiple Choice";
+            break;
+          case 3:
+            aType = "Short Answer";
+            break;
+          case 4:
+            aType = "Long Answer";
+            break;
+          default:
+            break;
+        }
+        aLabel.innerHTML = aType;
+        aRadioButton.setAttribute("value", aType);
+        aLabel.appendChild(aRadioButton);
+        container.appendChild(aLabel);
+      }
+      
+      // add final question element to form
+      document.getElementById("questionGroupContainer").appendChild(container);
+    }
 }
